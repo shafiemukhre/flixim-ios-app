831d9088e011ab139c056b391370a6735218e535
shafie <shafiemukhre@gmail.com>
2019-02-08 01:43:25 -0800

    my changes

---
+++ flixim/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  flixim
+//
+//  Created by Shafie Mukhre on 2/8/19.
+//  Copyright © 2019 Shafie. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
